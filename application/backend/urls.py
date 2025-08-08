a195e55e8dff333ecbda011ad1fe0d8e03d8bcdd
CastroClarence <21-00390@g.batstate-u.edu.ph>
2025-08-09 02:53:23 +0800

    service CRUD

--- application/backend/urls.py
+++ application/backend/urls.py
@@ -1,7 +1,13 @@
 from backend import views
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'services', views.ServiceView, basename='service')
+
 urlpatterns = [
     path('user/', views.UserView.as_view()),
     path('user/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
-    path('user/register/', views.RegisterView.as_view())
+    path('user/register/', views.RegisterView.as_view()),
+    path('', include(router.urls))
 ]
--- application/backend/views.py
+++ application/backend/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import views, generics, permissions
+from rest_framework import views, generics, permissions, viewsets
 from django.contrib.auth.models import User
 from backend import serializers
+from backend.permissions import IsOwnerOrReadOnly
+from backend.models import Service
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
@@ -16,3 +18,11 @@ class UserDetailView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+class ServiceView(viewsets.ModelViewSet):
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Service.objects.all()
+    serializer_class = serializers.ServiceSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
