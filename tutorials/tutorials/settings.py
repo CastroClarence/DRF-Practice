d97a15024d329c713cd69dd5b0a07acac32dd38e
CastroClarence <21-00390@g.batstate-u.edu.ph>
2025-08-08 14:10:54 +0800

    Tutorial 2 - Request and Response

--- tutorials/tutorials/settings.py
+++ tutorials/tutorials/settings.py
@@ -41,12 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES' : [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
